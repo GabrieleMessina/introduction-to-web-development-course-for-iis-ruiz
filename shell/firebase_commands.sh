01752d161a201bfb81b5b158902f26653fb46583
Gabriele Messina <g.messina@neoteksolutions.it>
2023-05-06 16:52:19 +0200

    Add test command to firebase shell file

--- shell/firebase_commands.sh
+++ shell/firebase_commands.sh
@@ -3,9 +3,12 @@
 # 2. Scaricare la CLI Firebase per poter sfruttare i loro comandi da Shell
 
 # 3. Collega la cartella corrente al progetto Firebase create dalla loro dashboard
-firebase init hosting
+firebase init
 
-# 4. Caricare i file del progetto su Firebase
-firebase deploy --only hosting
+# 4. Testiamo il nostro sito in locale prima della pubblicazione
+firebase emulators:start 
 
-# 5. Et voilà andiamo a vedere cosa è successo sulla dashboard
+# 5. Caricare i file del progetto su Firebase
+firebase deploy
+
+# 6. Et voilà andiamo a vedere cosa è successo sulla dashboard
